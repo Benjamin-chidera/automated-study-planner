ea4870d4b307a6cd3c27dd30554a8a4e9c45841e
MAC <mac@MACs-MacBook-Pro-3.local>
2025-06-21 20:46:13 +0100

    refactor: changed logos

--- src/components/profile/delete/profile-deleted.tsx
+++ src/components/profile/delete/profile-deleted.tsx
@@ -14,6 +14,7 @@ import {
 } from "@/components/ui/dialog";
 import { DialogClose } from "@radix-ui/react-dialog";
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 interface ProfileProps {
   user: SessionPayload | undefined;
@@ -41,10 +42,12 @@ export const ProfileDeleted = ({ user }: ProfileProps) => {
               `Logout failed: ${response.status} ${response.statusText}`
             );
           }
+          toast.success(state.message);
           window.location.href = "/login";
         })
         .catch((error) => {
           console.error("Failed to log out:", error);
+          toast.error("Failed to log out");
           window.location.href = "/login"; // Redirect anyway
         });
     }
